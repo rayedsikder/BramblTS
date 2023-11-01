fcf2a1a5a2e013b0a04f6cc961fd0388af0547d1
Md. Shahid-ul Islam <shahid@Mds-MacBook-Pro.local>
2023-11-01 13:29:18 +0600

    Update test file

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Ed25519 from './crypto/signing/ed25519/ed25519';
 import { SecretKey } from './crypto/signing/ed25519/ed25519_spec';
 
 function uint8ArrayToHexString(uint8Array: Uint8Array): string {
-  return Array.from(uint8Array, (byte) => byte.toString(16).padStart(2, '0')).join('');
+  const hex = Buffer.from(uint8Array).toString('hex');
+  return hex;
 }
 
 function stringToUint8Array(str: string): Uint8Array {
@@ -17,6 +19,9 @@ function stringToUint8Array(str: string): Uint8Array {
   return uint8Array;
 }
 
+const fromHexString = (hexString: any) =>
+  Uint8Array.from(hexString.match(/.{1,2}/g).map((byte: any) => parseInt(byte, 16)));
+
 const checkSign = new Ed25519().sign(
   new SecretKey(stringToUint8Array('9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60')),
   stringToUint8Array(''),
@@ -24,4 +29,11 @@ const checkSign = new Ed25519().sign(
 
 const checkHex = uint8ArrayToHexString(checkSign);
 
-console.log('checking signature', checkSign, checkHex, new SecretKey(stringToUint8Array('9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60')))
+console.log(
+  'checking signature',
+  checkSign,
+  checkHex,
+  new SecretKey(stringToUint8Array('9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60')),
+);
+
+console.log('from string...', fromHexString('9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60'));
